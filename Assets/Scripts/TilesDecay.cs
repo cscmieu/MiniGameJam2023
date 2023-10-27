5b6f7b91eaac6226fcbf150a069f1e39f2f9e9f7
benjamin_farigoul <ben.farigoul@gmail.com>
2023-10-27 13:43:53 -0400

    2D package

---
+++ Assets/Scripts/TilesDecay.cs
@@ -0,0 +1,23 @@
+using System;using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TilesDecay : MonoBehaviour
+{
+    public GameObject right;
+    public GameObject left;
+    public GameObject up;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
