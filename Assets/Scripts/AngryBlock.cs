11002ebca3d03e527955e0c8cc12560c4386bac9
Raton <149146535+Raton-mng@users.noreply.github.com>
2023-10-28 19:18:40 +0200

    a

--- Assets/Scripts/AngryBlock.cs
+++ Assets/Scripts/AngryBlock.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class AngryBlock : MonoBehaviour
 {
     private bool stop = true;
+    public Animator animator;
     [SerializeField] private BoxCollider2D detection;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.CompareTag("Player"))
         {
-            //PlayerMovement.Knockback();
+            EffectManager.KnockBackTriggered = true;
         }
-        Debug.LogAssertion("touche");
         stop = true;
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
         detection.enabled = false;
@@ -21,7 +21,7 @@ public class AngryBlock : MonoBehaviour
         stop = (!(detection.IsTouchingLayers(LayerMask.GetMask("Player"))) && stop);
         if (!stop)
         {
-            Vector2 veloc = new Vector2(0.2f * gameObject.transform.right.x + GetComponent<Rigidbody2D>().velocity.x, 0.2f * gameObject.transform.right.y + GetComponent<Rigidbody2D>().velocity.y);
+            Vector2 veloc = new Vector2(-0.1f * gameObject.transform.right.x + GetComponent<Rigidbody2D>().velocity.x, -0.1f * gameObject.transform.right.y + GetComponent<Rigidbody2D>().velocity.y);
             GetComponent<Rigidbody2D>().velocity = veloc;
         }
     }
